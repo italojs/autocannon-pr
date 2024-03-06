169721151877e79115139d44ef3f0182a91cdc8f
italo jose <italo.i@live.com>
2024-03-06 19:08:09 -0300

    wip

--- stress-test.js
+++ stress-test.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 
 function runStressTest() {
   const stream = fs.createWriteStream('results.md');
+  stream.write('```');
+  stream.on('finish', () => {
+    stream.write('```');
+  });
   const instance = autocannon({
     url: 'http://localhost:8000', 
     connections: 100, 
@@ -10,6 +14,8 @@ function runStressTest() {
   }, console.log);
 
   autocannon.track(instance, { outputStream: stream });
+  // add ``` to the beginning and end of the file
+
 }
 
 runStressTest();
